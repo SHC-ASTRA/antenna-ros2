cd51e246107f9273d1692283dc20304f7b35e757
Esten Readel <dragonkeeper0419@gmail.com>
2025-11-15 01:13:12 -0600

    Basic serial relay and COBS setup

--- src/antenna_pkg/src/antenna_node.py
+++ src/antenna_pkg/src/antenna_node.py
@@ -1,106 +1,144 @@
-from rclpy.node import Node, Publisher, Subscription
-from serial import Serial
-from sensor_msgs.msg import Imu, NavSatFix, NavSatStatus, MagneticField
-from glob import glob
-from os import getenv
-from time import sleep
-from threading import Thread
-import sys
-import signal
-import atexit
 import rclpy
+from rclpy.node import Node
+from rclpy import qos
 
-anchor_node: "None | AnchorNode" = None
-thread: None | Thread = None
+import signal
+import time
+import struct
 
+import serial
+import sys
+import glob
 
-class AnchorNode(Node):
-    imu_pub: Publisher
-    sat_pub: Publisher
-    rover_sat_sub: Subscription
-    serial: Serial
+from sensor_msgs.msg import NavSatFix,NavSatStatus
 
+serial_pub = None
+
+NUM_SERIAL_PORTS = 3
+BAUD_RATE = 115200
+
+class SerialRelay(Node):
     def __init__(self):
-        super().__init__("tracking_antenna")
+        super().__init__("antenna_node")
 
-        # Create publishers
-        self.imu_pub = self.create_publisher(Imu, "/antenna/feedback/imu", 10)
-        self.sat_pub = self.create_publisher(NavSatFix, "/antenna/feedback/sat", 10)
+        self.gps_sub_ = self.create_subscription(
+            NavSatFix, '/gps/fix', self.gps_callback, qos_profile=qos.qos_profile_sensor_data
+        )      
 
-        self.rover_sat_sub = self.create_subscription(
-            NavSatFix, "/core/gps", self.send_angle, 10
-        )
 
-        self.port = getenv("PORT_OVERRIDE")
-
-        # try 4 times
-        for _ in range(4):
-            # if we have a port already we can skip
-            if self.port is not None:
-                break
-            # test all possible ports
-            for port in glob("/dev/ttyUSB*") + glob("/dev/ttyACM*"):
+        ports = SerialRelay.list_serial_ports()
+        for i in range(NUM_SERIAL_PORTS):
+            for port in ports:
                 try:
-                    ser = Serial(port, 115200, timeout=1)
+                    ser = serial.Serial(port, BAUD_RATE, timeout=1)
                     ser.write(b"ping\n")
-                    res = ser.read_until(bytes("\n", "utf8"))
+                    response = ser.read_until(bytes("\n", "utf8"))
 
-                    # if we didn't get a pong back, we aren't talking to the right thing
-                    if b"pong" not in res:
-                        continue
-
-                    self.port = port
-                    self.get_logger().info(f"Found MCU at {self.port}")
+                    if b"pong" in response:
+                        self.port = port
+                        self.get_logger().info(f"MCU found at {self.port}")
+                        break
                 except:
                     pass
 
         if self.port is None:
             self.get_logger().info("Unable to find MCU...")
-            sleep(1)
-            exit(1)
+            time.sleep(1)
+            sys.exit(1)
 
-        self.serial = Serial(self.port, 115200)
+        self.ser = serial.Serial(self.port,BAUD_RATE)
 
-        atexit.register(self.cleanup)
 
-    def run(self):
-        global thread
-        thread = Thread(target=rclpy.spin, args={self}, daemon=True)
-        thread.start()
+    @staticmethod
+    def list_serial_ports():
+        return glob.glob("/dev/ttyUSB*") + glob.glob("/dev/ttyACM*")
+    
+    
+    def cleanup(self):
+        self.get_logger().info("Cleaning up")
+        if self.ser.is_open:
+            self.ser.close()
+
+
+    def gps_callback(self, msg: NavSatFix):
+        latitude = msg.latitude
+        longitude = msg.longitude
+
+        if msg.status.status == NavSatStatus.STATUS_NO_FIX:
+            self.get_logger().warn("Waiting for GPS Fix")
+            return
+        
+        try:
+            msg_bytes = struct.pack('<ff', latitude, longitude) # 8 bytes
+
+            checksum_value = self.sum_checksum(msg_bytes)
+            checksum_byte = struct.pack('<B', checksum_value) # 1 byte
+
+            packet_data = msg_bytes + checksum_byte # 9 bytes
+
+            encoded_packet = self.cobs_endode(packet_data) # 10 bytes
+
+        except Exception as e:
+            print(f"Couldn't encode message: {e}")
+
 
         try:
-            while rclpy.ok():
-                self.read_mcu()
-        except KeyboardInterrupt:
-            exit(0)
+            self.ser.write(encoded_packet + 0x00) # 11 bytes, packet delimiter
+            self.get_logger().info(f"Sent to MCU: {latitude}, {longitude} as {encoded_packet}")
 
-    def read_mcu(self):
-        pass
-
-    def send_angle(self, msg: NavSatFix):
-        pass
-
-    def cleanup(self):
-        pass
+        except serial.SerialException as e:
+            self.get_logger().warn(f"Couldn't write to serial {e}")
 
 
-def my_except_hook(type, value, traceback):
-    print("Uncaught exception:", type, value)
-    if anchor_node:
-        anchor_node.cleanup()
+    # Consistent Overhead Byte Stuffing
+    def cobs_endode(data: bytes) -> bytes:
+        # mutable bytes array
+        out = bytearray()
 
+        # first byte starts as pointing 1 ahead
+        out.append(0x01)
+        pointer_index = 0
+        pointer_distance = 1
+
+        for b in data:
+            if b == 0x00:
+                # set pointer byte to proper distance
+                out[pointer_index] = pointer_distance
+                # start pointer byte from current byte position
+                out.append(0x01)
+                pointer_index = len(out)
+                pointer_distance = 1
+            else:
+                out.append(b)
+                pointer_distance += 1
+        # set last pointer byte to proper distance (to future delimiter byte)
+        out[pointer_index] = pointer_distance
+
+        return out
+    
+    def sum_checksum(data: bytes) -> int:
+            return sum(data) & 0xFF
+
+def myexcepthook(type, value, traceback):
+    print("Uncaught exception", type, value)
+    if serial_pub:
+        serial_pub.cleanup()
 
 def main(args=None):
     rclpy.init(args=args)
-    sys.excepthook = my_except_hook
 
-    global anchor_node
+    global serial_pub
+    serial_pub = SerialRelay()
 
-    anchor_node = AnchorNode()
-    anchor_node.run()
+    try:
+        rclpy.spin(serial_pub)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        sys.exit(0)
 
-
-if __name__ == "__name__":
-    signal.signal(signal.SIGTERM, lambda signum, frame: exit(0))
+if __name__ == "__main__":
+    signal.signal(
+        signal.SIGTERM, lambda signum, frame: sys.exit(0)
+    )
     main()
-
